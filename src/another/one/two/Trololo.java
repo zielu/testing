b8a5b3bb50745b6757ea28691d1e7c0783eb3546
Lukasz Zielinski <zieluuuu@gmail.com>
2018-02-28 21:52:53 +0100

    Changed

---
+++ src/another/one/two/Trololo.java
@@ -0,0 +1,12 @@
+package another.one.two;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Lukasz
+ * Date: 09.02.13
+ * Time: 13:54
+ * To change this template use File | Settings | File Templates.
+ * AAAAAA
+ */
+public class Trololo {
+}
